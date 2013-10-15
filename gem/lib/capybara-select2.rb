7058f6f96ba6fc5df5d109d2bbf4b49e471c0767
François VANTOMME <akarzim@gmail.com>
2013-10-15 11:53:13 +0200

    fix cops and deprecated method calls

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -1,70 +1,70 @@
-require "capybara-select2/version"
+require 'capybara-select2/version'
 require 'rspec/core'
 
 module Capybara
   module Select2
     def select2(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+      fail "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) && [:from, :xpath].any? { |k| options.key? k }
 
-      if options.has_key? :xpath
+      if options.key? :xpath
         select2_container = first(:xpath, options[:xpath])
       else
         select_name = options[:from]
-        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+        select2_container = first('label', text: select_name).find(:xpath, '..').find '.select2-container'
       end
 
-      select2_container.find(".select2-choice").click
+      select2_container.find('.select2-choice').click
 
-      if options.has_key? :search
-        find(:xpath, "//body").find("input.select2-input").set(value)
-        page.execute_script(%|$("input.select2-input:visible").keyup();|)
-        drop_container = ".select2-results"
+      if options.key? :search
+        find(:xpath, '//body').find('input.select2-input').set value
+        page.execute_script '$("input.select2-input:visible").keyup();'
+        drop_container = '.select2-results'
       else
-        drop_container = ".select2-drop"
+        drop_container = '.select2-drop'
       end
 
-      find(:xpath, "//body").find("#{drop_container} li", text: value).click
+      find(:xpath, '//body').find("#{drop_container} li", text: value).click
     end
 
     def select2_ajax(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+      fail "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) && [:from, :xpath].any? { |k| options.key? k }
 
-      if options.has_key? :xpath
+      if options.key? :xpath
         select2_container = first(:xpath, options[:xpath])
       else
         select_name = options[:from]
-        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+        select2_container = first('label', text: select_name).find(:xpath, '..').find '.select2-container'
       end
 
-      select2_container.find(".select2-choice").click
+      select2_container.find('.select2-choice').click
 
       drop_container = '.select2-drop'
-      if options.has_key? :search
-        if options.has_key? :parent
-          find(:xpath, "//body").find("#{options[:parent]} input.select2-input").set(value)
+      if options.key? :search
+        if options.key? :parent
+          find(:xpath, '//body').find("#{options[:parent]} input.select2-input").set value
           page.execute_script(%|$("#{options[:parent]} input.select2-input:visible").keyup();|)
         else
-          find(:xpath, "//body").find("input.select2-input").set(value)
-          page.execute_script(%|$("input.select2-input:visible").keyup();|)
+          find(:xpath, '//body').find('input.select2-input').set value
+          page.execute_script '$("input.select2-input:visible").keyup();'
         end
       end
 
-      find(:xpath, "//body").find("#{drop_container} li", text: value).click
+      find(:xpath, '//body').find("#{drop_container} li", text: value).click
     end
 
-    def select2_multiple(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+    def select2_multiple(values, options = {})
+      fail "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) && [:from, :xpath].any? { |k| options.key? k }
 
-      if options.has_key? :xpath
+      if options.key? :xpath
         select2_container = first(:xpath, options[:xpath])
       else
         select_name = options[:from]
-        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+        select2_container = first('label', text: select_name).find(:xpath, '..').find '.select2-container'
       end
 
-      [value].flatten.each do |value|
+      [values].flatten.each do |value|
         select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
-        find(:xpath, "//body").find(".select2-drop li", text: value).click
+        find(:xpath, '//body').find('.select2-drop li', text: value).click
       end
     end
   end
