b6edb7e78a53ef9fff45938b56d75c8f1de4e44b
François VANTOMME <akarzim@gmail.com>
2013-10-15 12:50:58 +0200

    wait until drop-mask has not disappeared

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -11,14 +11,16 @@ module Capybara
 
       select2_container.find('.select2-choice').click
 
-      if search?
+      if search
         select2_input = 'input.select2-input'
         find(:xpath, '//body').find(select2_input).set value
         page.execute_script(%|$("#{select2_input}:visible").keyup();|)
       end
 
-      drop_container = search? ? '.select2-results' : '.select2-drop'
+      drop_container = search ? '.select2-results' : '.select2-drop'
       find(:xpath, '//body').find("#{drop_container} li", text: value).click
+
+      fail unless page.has_no_css? '#select2-drop-mask'
     end
 
     def select2_ajax(value, from: nil, xpath: nil, search: false, parent: '')
@@ -29,14 +31,16 @@ module Capybara
 
       select2_container.find('.select2-choice').click
 
-      if search?
+      if search
         select2_input = "#{parent} input.select2-input".strip
         find(:xpath, '//body').find(select2_input).set value
         page.execute_script(%|$("#{select2_input}:visible").keyup();|)
       end
 
-      drop_container = search? ? '.select2-results' : '.select2-drop'
+      drop_container = search ? '.select2-results' : '.select2-drop'
       find(:xpath, '//body').find("#{drop_container} li", text: value).click
+
+      fail unless page.has_no_css? '#select2-drop-mask'
     end
 
     def select2_multiple(values, from: nil, xpath: nil)
@@ -49,6 +53,8 @@ module Capybara
         select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
         find(:xpath, '//body').find('.select2-drop li', text: value).click
       end
+
+      fail unless page.has_no_css? '#select2-drop-mask'
     end
   end
 end
